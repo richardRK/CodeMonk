08ced91d40bca9caba939ad32e3a90c6d1b90b9f
rohi <richardRK@users.noreply.github.com>
2020-12-22 15:05:20 +0530

    Initial Program

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-
-namespace ConsoleApp1
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
 {
-    class Program
+
+    static void Main(String[] args)
     {
-        static void Main(string[] args)
+        int T = Convert.ToInt16(Console.ReadLine());
+
+        while(T-->0)
         {
-            Console.WriteLine("Hello World!");
+            string NandK = Console.ReadLine();
+            int N = Convert.ToInt32(NandK.Split()[0]);
+            int K = Convert.ToInt32(NandK.Split()[1]);
+            int[] items = new int[N];
+            items = Console.ReadLine().Split().Select(Int32.Parse).ToArray();
+
+            int[] result = shiftRight(items, K);
+
+            Console.WriteLine(string.Join(" ", result));
         }
+        Console.ReadLine();
     }
+
+    public static int[] shiftRight(int[] arr, int K)
+    {
+        int[] demo = new int[arr.Length];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            demo[(i + K) % demo.Length] = arr[i];
+        }
+        return demo;
+    }
+
+
+
+
+
+
 }
