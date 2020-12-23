b94a73446f1eba090396517da0cebf4d872efaeb
rohi <richardRK@users.noreply.github.com>
2020-12-23 15:57:39 +0530

    cd

--- ConsoleApp1/Test.cs
+++ ConsoleApp1/Test.cs
@@ -2,60 +2,35 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-class Solution
+public class Test
 {
 
     static void Main(String[] args)
     {
-        long T = Convert.ToInt64(Console.ReadLine());
-        long i, j;
-        long count = 0;
-        string[] arr = null;
-
-        while (T-- > 0)
-        {
-            long N = Convert.ToInt64(Console.ReadLine());
-
-            long[,] arr1 = new long[N, N];
-
-            for (i = 0; i < N; i++)
-            {
-                arr = (Console.ReadLine().Split(' '));
-
-                for (j = 0; j < N; j++)
-                {
-                    arr1[i, j] = long.Parse(arr[j]);
-
-                }
-            }
-
-            for (i = 0; i < N; i++)
-            {
-                for (j = 0; j < N; j++)
-                {
-                    for (long x = i; x < N; ++x)
-                    {
-                        for (long y = j; y < N; ++y)
-                        {
-                            if (arr1[x, y] < arr1[i, j])
-
-                                count++;
-                        }
-
-                    }
-                }
-            }
-            Console.WriteLine(count);
-        }
-
-
-        Console.ReadLine();
+        var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int N = Convert.ToInt32(Console.ReadLine());
+        TwoSum(input, N);
     }
 
+    public static int[] TwoSum(int[] nums, int target)
+    {
 
 
 
 
+    }
+
+    private static string leftRotateShift(string B, int shift)
+    {
+        shift %= B.Length;
+        return B.Substring(shift) + B.Substring(0, shift);
+    }
+
+    private static string rightRotateShift(string B, int shift)
+    {
+        shift %= B.Length;
+        return B.Substring(B.Length - shift) + B.Substring(0, B.Length - shift);
+    }
 
 
 
