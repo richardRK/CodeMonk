b2d1f24aec0ef1cedd71b1f1a90af766e7f989b0
rohi <richardRK@users.noreply.github.com>
2020-12-25 12:07:07 +0530

    LeetCode

--- ConsoleApp1/Test.cs
+++ ConsoleApp1/Test.cs
@@ -7,31 +7,52 @@ public class Test
 
     static void Main(String[] args)
     {
-        var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-        var res = RunningSum1DArray(input);
+        //var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        //var res = RunningSum1DArray(input);
+
+
+        int[][] studentIDS = new int[][] {
+                new int[] { 1, 91 },
+                new int[] { 1, 92 },
+                new int[] { 1, 60 },
+                new int[] { 1, 65 },
+                new int[] { 1, 87 },
+                new int[] { 1, 100 },
+                new int[] { 2, 93 },
+                new int[] { 2, 97 },
+                new int[] { 2, 77 },
+                new int[] { 2, 100 },
+                new int[] { 2, 76 }
+            };
+
+
+        HighFive(studentIDS);
     }
+
+    private static void HighFive(int[][] items)
+    {
+        Sort(items, 1);
+    }
+    private static void Sort<T>(T[][] data, int col)
+    {
+        Comparer<T> comparer = Comparer<T>.Default;
+        Array.Sort<T[]>(data, (x, y) => comparer.Compare(x[col], y[col]));
+    }
+
 
     private static int[] RunningSum1DArray(int[] input)
     {
         int N = input.Length;
         List<int> lst = new List<int>();
-        int calcSum = 0;
+         int calcSum = 0;
         for (int i = 0; i < N; i++)
         {
-            if (i == 0)
-            {
-                lst.Add(input[i]);
-            }
-            else
-            {
                 for (int k = i; k >= 0; k--)
                 {
                     calcSum += input[k];
                 }
-
                 lst.Add(calcSum);
                 calcSum = 0;
-            }
         }
         return lst.ToArray();
     }
